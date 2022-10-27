368f32d0553ce997ae7041ad3bbbc3076d3c2c07
Amanda Fantuzze <105981624+FriendlyCheesecake@users.noreply.github.com>
2022-10-28 06:41:28 +0900

    Enums Idioma e Legenda

---
+++ Locadora/Idioma.java
@@ -0,0 +1,5 @@
+package Locadora;
+
+public enum Idioma {
+    PORTUGUES, ESPANHOL, FRANCES, INGLES, ALEMAO, JAPONES, COREANO
+}
---
+++ Locadora/IdiomaLegenda.java
@@ -0,0 +1,5 @@
+package Locadora;
+
+public enum IdiomaLegenda {
+    PORTUGUES, INGLES, ESPANHOL, SEMLEGENDA
+}
