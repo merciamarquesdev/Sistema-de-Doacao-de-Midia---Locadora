98f86eb441466c0323f941d4bab75c9b7e56a318
Eliane Discacciati <discacciati@gmail.com>
2022-10-28 17:59:55 -0300

    feat: class cd

--- Locadora/Cd.java
+++ Locadora/Cd.java
@@ -1,7 +1,43 @@
-//package Locadora;
-//
-//public class Cd extends Midia{
-//    private String artista;
-//    private int quantidadeFaixas;
-//    //continuar
-//}
+package Locadora;
+
+public class Cd extends Midia {
+    private String artista;
+    private int quantidadedeFaixas ;
+
+    //sobrescrever darPlay : Exibir a msg "Primeira faixa tocando"
+    public void darPlay(){
+        System.out.println("Primeira faixa tocando!");
+    }
+
+    //constructor
+    public Cd (String codigo, Double preco, String nome, String artista, int quantidadedeFaixas  ){
+        super(codigo, preco, nome);
+        this.artista = artista;
+        this.quantidadedeFaixas = quantidadedeFaixas;
+    }
+
+    //get e set
+    public String getArtista(){
+        return artista;
+    }
+
+    public void setArtista(String artista){
+        this.artista = artista;
+    }
+
+    public int getQuantidadedeFaixas(){
+        return quantidadedeFaixas;
+    }
+
+    public void setQuantidadedeFaixas(int quantidadedeFaixas){
+        this.quantidadedeFaixas = quantidadedeFaixas;
+    }
+
+
+    @Override
+    public String toString(){
+        return super.toString() + ", de "
+                + artista + ", com "
+                + quantidadedeFaixas + " faixas.";
+    }
+}
